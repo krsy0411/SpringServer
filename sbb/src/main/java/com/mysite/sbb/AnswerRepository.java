e0b098d7d96e463cd0c4247e21d2f79ebf2dad35
PANdas <krsy0411@naver.com>
2024-02-22 17:27:50 +0900

    2-05 리포지토리로 데이터베이스 관리하기

---
+++ sbb/src/main/java/com/mysite/sbb/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Answer entity로 repository 생성. 기본키는 Interger
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
