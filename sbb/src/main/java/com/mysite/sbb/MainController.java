e00ec1b29972497d905a353c1b47ad1a332cc78d
PANdas <krsy0411@naver.com>
2024-02-24 13:18:52 +0900

    [done] 2-08: 루트 URL 사용하기

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-
+	
+	// sbb url 매핑 
     @GetMapping("/sbb")
     @ResponseBody
     public String index() {
-        return "안녕하세요 sbb에 오신 것을 환영합니다.";
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+    
+    // 루트 url 매핑
+    @GetMapping("/")
+    public String root() {
+    	// /question/list URL로 페이지를 리다이렉트하라는 명령어  
+        return "redirect:/question/list";
     }
 }
